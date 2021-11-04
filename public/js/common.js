a112c53f7e4d806abbfae78b70b107dce67abb06
luckyone <luckyone1221@gmail.com>
2021-11-04 16:11:12 +0200

    first commit

--- public/js/common.js
+++ public/js/common.js
@@ -1,347 +1,158 @@
-"use strict";
-const JSCCommon = {
+window.addEventListener('load', function (){
 
-	btnToggleMenuMobile: [].slice.call(document.querySelectorAll(".toggle-menu-mobile--js")),
-	menuMobile: document.querySelector(".menu-mobile--js"),
-	menuMobileLink: [].slice.call(document.querySelectorAll(".menu-mobile--js ul li a")),
+  // kind of handMade Swiper with gasp, can improve this to infinity
+  // I would use simple ccs transition instead of gasp if it wasnt required
+  // nobody is saved from bugs, but I havent seen any
+  // u can reuse it many times with diff sett
+  function customSlider(containerSelector, sett){
+    let cont = document.querySelector(containerSelector);
+    let slider = {
+      //container
+      sliderCont: cont,
+      //slides
+      slides: cont.querySelectorAll('.custom-slide-js'),
+      //it take props from sett obj
+      transitionDuration: sett.transitionDuration || 1,
+      autoplay: sett.autoplay,
+      autoplayReverse: sett.autoplayReverse,
+      initialSlide: sett.initialSlide || 0,
+      keyBoardControll: sett.keyBoardControll,
+      arrowsPrev: document.querySelectorAll(sett.arrowsPrev || '.custom-prev-js'),
+      arrowsNext: document.querySelectorAll(sett.arrowsNext || '.custom-next-js'),
 
-	modalCall() {
-		const link = ".link-modal-js";
+      //
+      activeSlide: 0,
+      currentPositionX: 0,
+      getSliderWidth: function (){
+        let sliderW = 0;
+        for (let slide of this.slides){
+          sliderW += slide.offsetWidth;
+        }
+        return sliderW;
+      },
+      getSlideWidth: function (index=this.activeSlide){
+        return this.slides[index].offsetWidth;
+      },
+      slideNext: function (event, transitionDuration=this.transitionDuration){
+        let nextIndex = this.activeSlide + 1;
+        let Xcord = this.currentPositionX - this.getSlideWidth();
+        if (nextIndex >= this.slides.length){
+          nextIndex = 0;
+          Xcord = 0;
+        }
+        this.activeSlide = nextIndex;
+        this.currentPositionX = Xcord;
 
-		Fancybox.bind(link, {
-			arrows: false,
-			infobar: false,
-			touch: false,
-			infinite: false,
-			dragToClose: false,
-			type: 'inline',
-			autoFocus: false,
-			l10n: {
-				Escape: "Закрыть",
-				NEXT: "Вперед",
-				PREV: "Назад", 
-			}, 
-		}); 
-		document.querySelectorAll(".modal-close-js").forEach(el=>{
-			el.addEventListener("click", ()=>{
-				Fancybox.close();
-			})
-		})
-		// fancybox.defaults.backFocus = false;
-		const linkModal = document.querySelectorAll(link);
-		function addData() {
-			linkModal.forEach(element => {
-				element.addEventListener('click', () => {
-					let modal = document.querySelector(element.getAttribute("href"));
-					const data = element.dataset;
+        gsap.to(this.sliderCont, {x: Xcord, duration: transitionDuration, ease: "power1.inOut"});
+      },
+      slidePrev: function (event, transitionDuration=this.transitionDuration){
+        let nextIndex = this.activeSlide - 1;
+        let Xcord = this.currentPositionX + this.getSlideWidth();
 
-					function setValue(val, elem) {
-						if (elem && val) {
-							const el = modal.querySelector(elem)
-							el.tagName == "INPUT"
-								? el.value = val
-								: el.innerHTML = val;
-							// console.log(modal.querySelector(elem).tagName)
-						}
-					}
-					setValue(data.title, '.ttu');
-					setValue(data.text, '.after-headline');
-					setValue(data.btn, '.btn');
-					setValue(data.order, '.order');
-				})
-			})
-		}
-		if (linkModal) addData();
-	},
-	// /modalCall
-	toggleMenu() {
-		const toggle = this.btnToggleMenuMobile;
-		const menu = this.menuMobile;
-		document.addEventListener("click", function (event) {
-			const toggleEv = event.target.closest(".toggle-menu-mobile--js");
-			if (!toggleEv) return;
-			toggle.forEach(el => el.classList.toggle("on"));
-			menu.classList.toggle("active");
-			[document.body, document.querySelector('html')].forEach(el => el.classList.toggle("fixed")); 
-		}, { passive: true });
-	},
-	closeMenu() {
-		let menu = this.menuMobile;
-		if (!menu) return;
-		if (menu.classList.contains("active")) {
-			this.btnToggleMenuMobile.forEach(element => element.classList.remove("on"));
-			this.menuMobile.classList.remove("active");
-			[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed")); 
-		}
+        if (nextIndex < 0){
+          nextIndex = this.slides.length-1;
+          Xcord = (this.getSliderWidth() - this.getSlideWidth(this.slides.length-1)) * -1;
+        }
+        this.activeSlide = nextIndex;
+        this.currentPositionX = Xcord;
 
-	},
-	mobileMenu() {
-		if (!this.menuMobileLink) return;
-		this.toggleMenu();
-		document.addEventListener('mouseup', (event) => {
-			let container = event.target.closest(".menu-mobile--js.active"); // (1)
-			let link = event.target.closest(".menu-mobile .menu a"); // (1)
-			let toggle = event.target.closest('.toggle-menu-mobile--js.on'); // (1)
-			if (!container && !toggle) this.closeMenu();
-		}, { passive: true });
+        gsap.to(this.sliderCont, {x: Xcord, duration: transitionDuration, ease: "power1.inOut"});
+      },
+      slideTo: function (index, duration=0){
+        if (index > this.activeSlide){
+          while (index > this.activeSlide){
+            this.slideNext(undefined, duration);
+            //-this.activeSlide = this.activeSlide + 1;
+          }
+        }else{
+          while (index < this.activeSlide){
+            this.slidePrev(undefined, duration);
+          }
+        }
+      },
+      //
+      setKeyBoardControll: function (){
+        if (!this.keyBoardControll) return
 
-		window.addEventListener('resize', () => {
-			if (window.matchMedia("(min-width: 992px)").matches) this.closeMenu();
-		}, { passive: true });
-	},
-	// /mobileMenu
+        let slider = this;
+        let keyBoardContaroll = function (){
+          if (event.key === 'ArrowLeft'){
+            slider.slidePrev();
+          }
+          if (event.key === 'ArrowRight'){
+            slider.slideNext();
+          }
+        };
 
-	// tabs  .
-	tabscostume(tab) {
-		// const tabs = document.querySelectorAll(tab);
-		// const indexOf = element => Array.from(element.parentNode.children).indexOf(element);
-		// tabs.forEach(element => {
-		// 	let tabs = element;
-		// 	const tabsCaption = tabs.querySelector(".tabs__caption");
-		// 	const tabsBtn = tabsCaption.querySelectorAll(".tabs__btn");
-		// 	const tabsWrap = tabs.querySelector(".tabs__wrap");
-		// 	const tabsContent = tabsWrap.querySelectorAll(".tabs__content");
-		// 	const random = Math.trunc(Math.random() * 1000);
-		// 	tabsBtn.forEach((el, index) => {
-		// 		const data = `tab-content-${random}-${index}`;
-		// 		el.dataset.tabBtn = data;
-		// 		const content = tabsContent[index];
-		// 		content.dataset.tabContent = data;
-		// 		if (!content.dataset.tabContent == data) return;
+        this.sliderCont.addEventListener('mouseenter',function (){
+          window.addEventListener('keyup', keyBoardContaroll);
+        });
+        this.sliderCont.addEventListener('mouseleave',function (){
+          window.removeEventListener('keyup', keyBoardContaroll);
+        })
 
-		// 		const active = content.classList.contains('active') ? 'active' : '';
-		// 		// console.log(el.innerHTML);
-		// 		content.insertAdjacentHTML("beforebegin", `<div class="tabs__btn-accordion  btn btn-primary  mb-1 ${active}" data-tab-btn="${data}">${el.innerHTML}</div>`)
-		// 	})
+      },
+      init: function (){
+        if (this.initialSlide && this.initialSlide >= 0 && this.initialSlide < this.slides.length){
+          this.slideTo(this.initialSlide);
+        }
+        this.setKeyBoardControll();
 
+        //
+        for (let next of this.arrowsNext){
+          next.addEventListener('click', this.slideNext.bind(this));
+        }
+        for (let prev of this.arrowsPrev){
+          prev.addEventListener('click', this.slidePrev.bind(this));
+        }
 
-		// 	tabs.addEventListener('click', function (element) {
-		// 		const btn = element.target.closest(`[data-tab-btn]:not(.active)`);
-		// 		if (!btn) return;
-		// 		const data = btn.dataset.tabBtn;
-		// 		const tabsAllBtn = this.querySelectorAll(`[data-tab-btn`);
-		// 		const content = this.querySelectorAll(`[data-tab-content]`);
-		// 		tabsAllBtn.forEach(element => {
-		// 			element.dataset.tabBtn == data
-		// 				? element.classList.add('active')
-		// 				: element.classList.remove('active')
-		// 		});
-		// 		content.forEach(element => {
-		// 			element.dataset.tabContent == data
-		// 				? (element.classList.add('active'), element.previousSibling.classList.add('active'))
-		// 				: element.classList.remove('active')
-		// 		});
-		// 	})
-		// })
+        if (this.autoplay && Number.isInteger(this.autoplay)){
+          if (this.autoplayReverse){
+            window.setInterval(this.slidePrev.bind(this), this.autoplay);
+          }
+          else{
+            window.setInterval(this.slideNext.bind(this), this.autoplay);
+          }
+        }
+      },
+    };
+    slider.init();
+    return slider;
+  }
 
-		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-				.eq($(this).index()).fadeIn().addClass('active');
+  let banerSlider = customSlider('.custom-slider-js', {
+    arrowsPrev: '.custom-prev1-js',
+    arrowsNext: '.custom-next1-js',
 
-		});
+    //
+    keyBoardControll: true,
+    //transitionDuration: 3,
+    //initialSlide: 2,
+    //autoplay: 3000,
+    //autoplayReverse: true,
+  });
+  console.log(banerSlider.sliderCont);
 
-	},
-	// /tabs
+  let banerSlider2 = customSlider('.custom-slider2-js', {
+    arrowsPrev: '.custom-prev2-js',
+    arrowsNext: '.custom-next2-js',
+    //
+    keyBoardControll: true,
+    transitionDuration: 0.5,
+    initialSlide: 2,
+    autoplay: 5000,
+    autoplayReverse: true,
+  });
+  console.log(banerSlider2.slides);
 
-	inputMask() {
-		// mask for input
-		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
-		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
-		Inputmask("+9(999)999-99-99").mask(InputTel);
-	},
-	// /inputMask
-	ifie() {
-		var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-		if (isIE11) {
-			document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
-		}
-	},
-	sendForm() {
-		var gets = (function () {
-			var a = window.location.search;
-			var b = new Object();
-			var c;
-			a = a.substring(1).split("&");
-			for (var i = 0; i < a.length; i++) {
-				c = a[i].split("=");
-				b[c[0]] = c[1];
-			}
-			return b;
-		})();
-		// form
-		$(document).on('submit', "form", function (e) {
-			e.preventDefault();
-			const th = $(this);
-			var data = th.serialize();
-			th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
-			th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
-			th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
-			th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
-			$.ajax({
-				url: 'action.php',
-				type: 'POST',
-				data: data,
-			}).done(function (data) {
+  //methods and property examples
+  //console.log(banerSlider.getSliderWidth());
+  //console.log(banerSlider.getSlideWidth());
+  //console.log(banerSlider.slideNext());
+  //console.log(banerSlider.slidePrev());
+  //console.log(banerSlider.slideTo());
 
-				Fancybox.close();
-				Fancybox.show([{ src: "#modal-thanks", type: "inline" }]);
-				// window.location.replace("/thanks.html");
-				setTimeout(function () {
-					// Done Functions
-					th.trigger("reset");
-					// $.magnificPopup.close();
-					// ym(53383120, 'reachGoal', 'zakaz');
-					// yaCounter55828534.reachGoal('zakaz');
-				}, 4000);
-			}).fail(function () { });
+  //console.log(banerSlider.sliderCont);
+  //console.log(banerSlider.slides);
 
-		});
-	},
-	heightwindow() {
-		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-		let vh = window.innerHeight * 0.01;
-		// Then we set the value in the --vh custom property to the root of the document
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-		// We listen to the resize event
-		window.addEventListener('resize', () => {
-			// We execute the same script as before
-			let vh = window.innerHeight * 0.01;
-			document.documentElement.style.setProperty('--vh', `${vh}px`);
-		}, { passive: true });
-	},
-	animateScroll() {
-		$(document).on('click', " .menu li a, .scroll-link", function () {
-			const elementClick = $(this).attr("href");
-			if (!document.querySelector(elementClick)) {
-				$(this).attr("href", '/' + elementClick)
-			}
-			else {
-				let destination = $(elementClick).offset().top;
-				$('html, body').animate({ scrollTop: destination - 80 }, 0);
-				return false;
-			}
-		});
-	},
-	getCurrentYear(el) {
-		let now = new Date();
-		let currentYear = document.querySelector(el);
-		if (currentYear) currentYear.innerText = now.getFullYear();
-	},
-	toggleShow(toggle, drop) {
-
-		let catalogDrop = drop;
-		let catalogToggle = toggle;
-
-		$(document).on('click', catalogToggle, function () {
-			$(this).toggleClass('active').next().fadeToggle('fast', function () {
-				$(this).toggleClass("active")
-			});
-		})
-
-		document.addEventListener('mouseup', (event) => {
-			let container = event.target.closest(catalogDrop + ".active"); // (1)
-			let link = event.target.closest(catalogToggle); // (1)
-			if (!container || !catalogToggle) {
-				$(catalogDrop).removeClass('active').fadeOut();
-				$(catalogToggle).removeClass('active');
-			};
-		}, { passive: true });
-	}
-};
-const $ = jQuery;
-
-function eventHandler() {
-	// JSCCommon.ifie();
-	JSCCommon.modalCall();
-	// JSCCommon.tabscostume('tabs');
-	// JSCCommon.mobileMenu();
-	// JSCCommon.inputMask();
-	// JSCCommon.sendForm();
-	JSCCommon.heightwindow();
-	// JSCCommon.toggleShow(".catalog-block__toggle--desctop", '.catalog-block__dropdown');
-	// JSCCommon.animateScroll();
-	
-	// JSCCommon.CustomInputFile(); 
-	var x = window.location.host;
-	let screenName;
-	screenName = document.body.dataset.bg;
-	if (screenName && x.includes("localhost:30")) {
-		document.body.insertAdjacentHTML("beforeend", `<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
-	}
-
-
-	function setFixedNav() {
-		let topNav = document.querySelector('.top-nav  ');
-		if (!topNav) return;
-		window.scrollY > 0
-			? topNav.classList.add('fixed')
-			: topNav.classList.remove('fixed');
-	}
-
-	function whenResize() {
-		setFixedNav();
-	}
-
-	window.addEventListener('scroll', () => {
-		setFixedNav();
-
-	}, { passive: true })
-	window.addEventListener('resize', () => {
-		whenResize();
-	}, { passive: true });
-
-	whenResize();
-
-
-	let defaultSl = {
-		spaceBetween: 0,
-		lazy: {
-			loadPrevNext: true,
-		},
-		watchOverflow: true,
-		spaceBetween: 0,
-		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		pagination: {
-			el: ' .swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-			// renderBullet: function (index, className) {
-			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
-			// }
-		},
-	}
-
-	const swiper4 = new Swiper('.sBanners__slider--js', {
-		// slidesPerView: 5,
-		...defaultSl,
-		slidesPerView: 'auto',
-		freeMode: true,
-		loopFillGroupWithBlank: true,
-		touchRatio: 0.2,
-		slideToClickedSlide: true,
-		freeModeMomentum: true,
-
-	});
-	// modal window
-
-};
-if (document.readyState !== 'loading') {
-	eventHandler();
-} else {
-	document.addEventListener('DOMContentLoaded', eventHandler);
-}
-
-// window.onload = function () {
-// 	document.body.classList.add('loaded_hiding');
-// 	window.setTimeout(function () {
-// 		document.body.classList.add('loaded');
-// 		document.body.classList.remove('loaded_hiding');
-// 	}, 500);
-// }
+})
